6ef9b653e6ea2027b3221bc65c3b4ab1d2cc19b9
evans.jiang <evans.jiang@gmail.com>
2024-08-30 16:28:33 +0800

    init

---
+++ goal/lib/goal.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class GoalPage extends StatefulWidget {
+  const GoalPage({super.key});
+
+  @override
+  _GoalPageState createState() => _GoalPageState();
+}
+
+class _GoalPageState extends State<GoalPage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: const Text('Goal Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ goal/lib/pool.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class PoolPage extends StatefulWidget {
+  const PoolPage({super.key});
+
+  @override
+  _PoolPageState createState() => _PoolPageState();
+}
+
+class _PoolPageState extends State<PoolPage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: const Text('Pool Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
